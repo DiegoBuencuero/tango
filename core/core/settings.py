aff52bf3d55cb3daf48708ded7a7cd3e0935857a
DiegoBuencuero <diegobuencuero4095@gmail.com>
2025-03-05 11:44:04 -0300

    correo

--- core/core/settings.py
+++ core/core/settings.py
@@ -132,12 +132,22 @@ STATICFILES_DIRS = [
 ]
 
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = ''
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'tango_services@outlook.ie'
+# EMAIL_HOST_PASSWORD = 'zffwwnwmhxdwkyqf'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = ''
+EMAIL_HOST = 'smtp.office365.com'  # Servidor SMTP para Outlook
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False  # No uses TLS y SSL juntos
 EMAIL_HOST_USER = 'tango_services@outlook.ie'
-EMAIL_HOST_PASSWORD = 'zffwwnwmhxdwkyqf'
+EMAIL_HOST_PASSWORD = 'zffwwnwmhxdwkyqf'  # ⚠️ Mueve esto a variables de entorno
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_TIMEOUT = 30  # Opcional: evita que el servidor cierre la conexión rápido
